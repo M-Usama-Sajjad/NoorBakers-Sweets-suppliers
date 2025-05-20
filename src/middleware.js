ab36ead64ddf7ece46dfbd91640d814dad2aa221
shehroz614 <shehrozmubarik9@gmail.com>
2025-05-20 07:30:40 -0500

    fixes

--- src/middleware.js
+++ src/middleware.js
@@ -3,11 +3,17 @@ import { NextResponse } from 'next/server'
 // List of public routes that don't require authentication
 const publicRoutes = ['/login', '/register', '/forgot-password']
 
+// List of public asset paths that should be accessible without authentication
+const publicAssets = ['/images/', '/assets/', '/media/', '/fonts/']
+
 export function middleware(request) {
   const { pathname } = request.nextUrl
   const token = request.cookies.get('token')?.value
 
-  console.log('token', token)
+  // Check if the path is for public assets
+  if (publicAssets.some(assetPath => pathname.startsWith(assetPath))) {
+    return NextResponse.next()
+  }
 
   // Check if the current route is a public route
   const isPublicRoute = publicRoutes.some(route => pathname.startsWith(route))
@@ -33,8 +39,14 @@ export const config = {
      * - api (API routes)
      * - _next/static (static files)
      * - _next/image (image optimization files)
+     * - _next/data (data files)
+     * - public (public files like robots.txt)
+     * - images (image files)
+     * - assets (asset files)
+     * - media (media files)
+     * - fonts (font files)
      * - favicon.ico (favicon file)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)'
+    '/((?!api|_next/static|_next/image|_next/data|public|images|assets|media|fonts|favicon.ico).*)'
   ]
 }
